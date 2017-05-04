58f54a35b73becdc8461f2e139bfe57c1d0b988e
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-05-04 11:20:12 -0400

    Update calc_bigrams.cpp
    Fixed comments again

--- calc_bigrams.cpp
+++ calc_bigrams.cpp
@@ -17,10 +17,10 @@ using namespace std;
 //professor Rivka Levitan, Brooklyn College Computer Science Department
 std::string normalizeString(const std::string& str) {
 	std::string res = str;						//1. Assign string input to res
-	std::cout << str << " ";					//2. Prints string with space in front of it
-	std::transform(res.begin(), res.end(), res.begin(), ::tolower);	//3. Sets string to lower case
-	res.erase(std::remove_if(res.begin(), res.end(), std::ptr_fun<int,int>(ispunct)), res.end());	//4. Removes punctuation
-	std:: cout << res << "\n";					//5. Prints newline in between.																//5. Inserts newline
+	std::cout << str << " ";					//2. Print string with space in front of it
+	std::transform(res.begin(), res.end(), res.begin(), ::tolower);	//3. Set string to lower case
+	res.erase(std::remove_if(res.begin(), res.end(), std::ptr_fun<int,int>(ispunct)), res.end());	//4. Remove punctuation
+	std:: cout << res << "\n";					//5. Print newline in between.
 	return res;
 }
 
@@ -31,16 +31,16 @@ int main(int argc, char* argv[]){
 	}
 	ifstream infile(argv[1]);
 
-	std::map<std::pair<std::string, std::string>, int> wordPairMap;	//Maps word pair count
-	std::map<std::string, int> wordMap;				//Maps word count
+	std::map<std::pair<std::string, std::string>, int> wordPairMap;	//Map word pair count.
+	std::map<std::string, int> wordMap;				//Map word count.
 	
 	std::string word1, word2;
 	std::pair<std::string, std::string> wordPair;
 	
-	infile >> word1;		//Initial reading. we have to start this off before we enter the loop
-	infile >> word2;		//because after this word 1 is going to be based off of word 2
+	infile >> word1;		//Initial reading. We have to start this off before we enter the loop
+	infile >> word2;		//because after this word 1 is going to be based off of word 2.
 
-	 wordPair.first = normalizeString(word1);		//Add the normalized strings to wordPair
+	 wordPair.first = normalizeString(word1);		//Add the normalized strings to wordPair.
 	 wordPair.second = normalizeString(word2);
 	 
 	 wordPairMap[wordPair]=1;			//Set the count	for the word pair.	
@@ -73,11 +73,11 @@ int main(int argc, char* argv[]){
 		else
 			cout << "\nFirst word not found" << endl;
 			
-		if(wordMap.count(b)){						//This checks if the second word is in the file.			
-			inputPair = std::make_pair(a, b);			//If it is, we assign the two words to a pair
-			if(wordPairMap.count(inputPair)){			//and check if the wordPair is in the map.
-				pairFreq = wordPairMap[inputPair];		//If it is in the map, we get frequency of the pair.
-				ratio = pairFreq / singleFreq;			//We then divide the pair frequency by the first-word frequency
+		if(wordMap.count(b)){					//This checks if the second word is in the file.			
+			inputPair = std::make_pair(a, b);		//If it is, we assign the two words to a pair
+			if(wordPairMap.count(inputPair)){		//and check if the wordPair is in the map.
+				pairFreq = wordPairMap[inputPair];	//If it is in the map, we get frequency of the pair.
+				ratio = pairFreq / singleFreq;		//We then divide the pair frequency by the first-word frequency
 				cout << "\nThe bigram probability is: " << ratio << endl; //to get the bigram probability.
 			}
 			else
